8f5d151bf03f907900dce40011c23743dc3ab866
Nakajima Kazuya <uniss010@gmail.com>
2017-07-27 21:05:44 +0900

    Refactored by using object destructuring

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
   static propTypes = {
@@ -17,22 +18,36 @@ class ListContacts extends Component {
   }
 
   render() {
+    const {contacts, onDeleteContact} = this.props
+    const {query} = this.state
+
+    let showingContacts
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), "i")
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
+    } else {
+      showingContacts = contacts
+    }
+
+    showingContacts.sort(sortBy("name"))
+
     return (
       <div className="list-contacts">
-        {JSON.stringify(this.state)}
-        
+        {/* Below code is to show the query value*/}
+        {/*{JSON.stringify(this.state)}*/}
+
         <div className="list-contacts-top">
           <input
             className="search-contacts"
             type="text"
             placeholder="Search contacts"
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
 
         <ol className="contact-list">
-          {this.props.contacts.map((contact) => 
+          {showingContacts.map((contact) => 
             <li key={contact.id} className="contact-list-item">
               <div className="contact-avatar" style={{
                 backgroundImage: `url(${contact.avatarURL})`
@@ -41,15 +56,13 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove">
+              <button onClick={() => onDeleteContact(contact)} className="contact-remove">
                 Remove
               </button>
             </li>
           )}
          </ol> 
       </div>
-
-      
     )  
   }
 }
