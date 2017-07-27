08d556297628f84bd87252a90b900798846a30f8
Nakajima Kazuya <uniss010@gmail.com>
2017-07-27 13:49:29 +0900

    added avatar, name, email and remove button

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 // this.props.contacts is array which has map() method.
 class ListContacts extends Component {
-	render() {
-		
-		return(
-			<ol className="contact-list">
-				{this.props.contacts.map((contact) => 
-					<li key={contact.id}>
-						{contact.name}
-					</li>
-					)}
-			</ol>
-		)
-	}
+  render() {
+  	return(
+        <ol className="contact-list">
+  			{this.props.contacts.map((contact) => 
+  				<li key={contact.id} className="contact-list-item">
+  					<div className="contact-avatar" style={{
+  						backgroundImage: `url(${contact.avatarURL})`
+  					}}/>
+  					<div className="contact-details">
+  						<p>{contact.name}</p>
+  						<p>{contact.email}</p>
+  					</div>
+            <button className="contact-remove">
+              Remove
+            </button>
+  				</li>
+  				)}
+  		</ol>
+  	)
+  }
 }
 
 export default ListContacts
