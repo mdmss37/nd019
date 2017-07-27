c1d33cf4bb1139c1be1995c13e14ddd0303791a9
Nakajima Kazuya <uniss010@gmail.com>
2017-07-27 12:19:11 +0900

    refactor the code to reuse contact list component

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -4,11 +4,7 @@ import React, { Component } from 'react';
 
 class ContactList extends React.Component {
   render() {
-    const people = [
-      { name: "Michael" },
-      { name: "Ryan" },
-      { name: "Tyler" }
-    ]
+    const people = this.props.contacts;
 
     return <ol>
       {people.map(person =>(
@@ -18,13 +14,21 @@ class ContactList extends React.Component {
   }
 }
 
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList/>
+        <ContactList contacts={[
+          { name : "Michael" },
+          { name : "Ryan" },
+          { name : "Tyler" }
+          ]}/>
+  
+        <ContactList contacts={[
+          { name : "Amanda" },
+          { name : "Richard" },
+          { name : "Jeff" }
+          ]}/>
       </div>
     );
   }
