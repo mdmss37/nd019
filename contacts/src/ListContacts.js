d6f7031c6052656831ed0ae6d45630b46922de71
Nakajima Kazuya <uniss010@gmail.com>
2017-07-27 13:32:37 +0900

    updated ListContacts to loop over contacts name

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-
+// this.props.contacts is array which has map() method.
 class ListContacts extends Component {
 	render() {
-		console.log("Props", this.props)
+		
 		return(
 			<ol className="contact-list">
+				{this.props.contacts.map((contact) => 
+					<li key={contact.id}>
+						{contact.name}
+					</li>
+					)}
 			</ol>
 		)
 	}
